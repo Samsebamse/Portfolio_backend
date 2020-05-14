c4ad18f155112024c493d62a6fd9f14b0b2569a9
samsebamse <megasamsi@gmail.com>
2020-05-14 02:28:04 +0200

    Initial commit #2

--- src/test/java/com/portfolio/demo/AuthorServiceIntegrationTest.java
+++ src/test/java/com/portfolio/demo/AuthorServiceIntegrationTest.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-
+@ActiveProfiles("test")
 @SpringBootTest
 public class AuthorServiceIntegrationTest extends SingletonTestDB {
 
